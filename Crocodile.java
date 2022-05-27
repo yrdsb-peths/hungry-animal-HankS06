edafd99a7129697e49cbdf7159b9c8c6a9e8d609
HankS06 <79720327+HankS06@users.noreply.github.com>
2022-05-26 20:19:37 -0400

    Add files via upload

--- Crocodile.java
+++ Crocodile.java
@@ -1,19 +1,87 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Crocodile here.
+ * The crocodile.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Crocodile extends Actor
 {
+    GreenfootSound crocodileSound = new GreenfootSound("minecrafteating.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[2];
+    GreenfootImage[] idleLeft = new GreenfootImage[2];
+    // Direction the crocodile is facing
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     /**
-     * Act - do whatever the Crocodile wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor - The code that gets run one time when object is created
      */
+    public Crocodile()
+    {
+        idleRight[0] = new GreenfootImage("images/idle0.png");
+        idleRight[0].scale(100,50);
+        idleRight[1] = new GreenfootImage("images/idle1.png");
+        idleRight[1].scale(100,50);
+        idleLeft[0] = new GreenfootImage("images/idle0.png");
+        idleLeft[0].mirrorHorizontally();
+        idleLeft[0].scale(100,50);
+        idleLeft[1] = new GreenfootImage("images/idle1.png");
+        idleLeft[1].mirrorHorizontally();
+        idleLeft[1].scale(100,50);
+        animationTimer.mark();
+        setImage(idleRight[0]);
+    }
+    /**
+     * Animate the crocodile
+     */
+    int imageIndex = 0;
+    public void animateCrocodile()
+    {
+        if(animationTimer.millisElapsed()<100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-2);
+            facing = "left";
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(2);
+            facing = "right";
+        }
+        //Remove fish if crocodile touches it
+        eat();
+        //Animate the crocodile
+        animateCrocodile();
+    }
+    /**
+     * Eat the fish and spawn new fish if the fish is eaten
+     */
+    public void eat()
+    {
+        if(isTouching(Fish.class))
+        {
+            removeTouching(Fish.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.spawnFish();
+            world.increaseScore();
+            crocodileSound.play();
+        }
     }
 }
